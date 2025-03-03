a4d62328153e8d1e1298e3e1309e5cd48c9d1ec2
Maikoleus <narushippumiguel@hotmail.com>
2025-03-03 11:43:18 -0600

    REVISION PERRROO CAMBIO CGG

--- Display.js
+++ Display.js
@@ -1,22 +1,56 @@
-class Display{
-constructor(displayAnterior,displayActual){
-    this.displayActual = displayValorActual;
-    this.displayAnterior = displayValorAnterior;
-    this.calculadora = new this.calculadora();
-    this.valorActual = "";
-    this.valorAnterior ="";
+class Display {
+    constructor(displayValorAnterior, displayValorActual) {
+        this.displayAnterior = displayValorAnterior;
+        this.displayActual = displayValorActual;
+        this.calculadora = new Calculadora();
+        this.valorActual = "";
+        this.valorAnterior = "";
+        this.Operacion = undefined;
+        this.signos = {
+            suma: '+',
+            resta: '-',
+            multi: '*',
+            div: '/',
+        }
+    }
 
+    escribirNumero(numero) {
+        if (numero =='.' && this.valorActual.includes('.')){return}
+        this.valorActual = this.valorActual.toString() + numero.toString(); // Concatenar los números en el display
+        this.imprimirValores();
+    }
+
+    imprimirValores() {
+        this.displayActual.textContent = this.valorActual;  
+        this.displayAnterior.textContent =  `${this.valorAnterior} ${this.signos[this.Operacion] || ''} `;
+    }
+borrar(){
+       this.valorActual = this.valorActual.toString().slice(0,-1);
+       this.imprimirValores();
+}
+
+
+borrarTodo(){
+
+    this.valorActual ="";
+    this.valorAnterior="";
+    this.Operacion = undefined;
+    this.imprimirValores();
+}
+calcular(){
+    const valorAnterior = parseFloat(this.valorAnterior);
+    const valorActual = parseFloat(this.valorActual);
+
+     if( isNaN( valorActual) || isNaN(valorAnterior)) return
+     this.valorActual = this.calculadora[ this.Operacion](valorAnterior, valorActual);
 
 }
-escribirNumero(numero){
-    this.valorActual= numero;
 
+computar(tipo){
+   this.Operacion !== 'igual' && this.calcular(); 
+   this.Operacion = tipo;
+   this.valorAnterior = this.valorActual || this.valorAnterior; 
+   this.valorActual ="";
+   this.imprimirValores();
 }
-
-imprimirValores(){
-    this.displayValorActual.texContent = this.valorActual;
-    this.displayValorAnterior.texContent = this.valorAnterior;
-
-}
-
 }
--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const displayValorAnterior= document.getElementById('val1');
-const displayValorActual= document.getElementById('val2');
-const botonesNumericos = document.querySelectorAll('.numero');
-const botonesOperadores = document.querySelectorAll('.operador');
+document.addEventListener("DOMContentLoaded", () => {
+  const displayValorAnterior = document.getElementById('val1');  
+  const displayValorActual = document.getElementById('val2');  
+  const botonesNumericos = document.querySelectorAll('.numero');  
+  const botonesOperadores = document.querySelectorAll('.operador');  
+  const botonBorrar = document.getElementById('borrar'); // Obtiene el botón de borrar
+  const botonBorrarTodo = document.getElementById('borrarTodo');
 
-const display = new Display(displayAnterior,displayActual);
-botonesNumericos.forEach(boton => {
-   boton.addEventListener('click', () => {
-     display.escribirNumero(boton.innerHTML);
-   });
+  const display = new Display(displayValorAnterior, displayValorActual);  
 
+  //  para escribir números
+  botonesNumericos.forEach(boton => {  
+      boton.addEventListener('click', () => {  
+          display.escribirNumero(boton.innerText);
+      });  
+  });
+
+  //  para borrar
+  botonBorrar.addEventListener('click', () => {
+      display.borrar();
+  });
+  botonBorrarTodo.addEventListener('click', () => {
+    display.borrarTodo();
 });
+
+botonesOperadores.forEach(boton =>{
+  boton.addEventListener('click', () => display.computar(boton.value))
+});
+});
+
